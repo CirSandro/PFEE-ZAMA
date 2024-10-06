038d75bc2e5be3081ead717f30d88912b4ab450a
Paul Giraud <paul.giraud@epita.fr>
2024-10-06 16:58:16 +0200

    test: ci working

--- ci/check_commits.sh
+++ ci/check_commits.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-# Pattern pour valider le message de commit
-COMMIT_MSG_REGEX="^:?(feat|fix|docs|style|refactor|test|chore): ?.+"
+# Commit message pattern
+COMMIT_MSG_REGEX="^(feat|fix|docs|style|refactor|test|chore): .+"
 
 commit_message="$1"
 
 if ! echo "$commit_message" | grep -qE "$COMMIT_MSG_REGEX"; then
   echo "Error: The commit message does not follow conventions."
-  echo "The message must begin with one of the following keywords: feat, fix, docs, style, refactor, test, chore."
+  echo "The message must begin with one of the following regex: ^(feat|fix|docs|style|refactor|test|chore): .+"
+  exit 1
+fi
+
+# Verify commit length
+if [ ${#commit_message} -gt 50 ]; then
+  echo "Error: The commit message exceeds 50 characters."
+  echo "Current length: ${#commit_message}. The maximum allowed is 50 characters."
   exit 1
 fi
